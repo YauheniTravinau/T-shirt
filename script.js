3689bf80be451bbcda50dd3a4a9d3da0f68d7691
YauheniTravinau <j.travinov@gmail.com>
2025-03-06 14:13:32 +0100

    333

--- script.js
+++ script.js
@@ -20,7 +20,27 @@ window.onclick = function(event) {
     }
 }
 
-document.getElementById('contactForm').addEventListener('submit', function(event) {
+document.getElementById('contactForm').addEventListener('submit', function() {
     // Показать сообщение только если форма отправлена успешно
     document.getElementById('responseMessage').style.display = 'block';
 });
+
+// Получаем кнопку
+let mybutton = document.getElementById("scrollToTopBtn");
+
+// Показываем кнопку при прокрутке
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+};
+
+// Функция прокрутки наверх
+mybutton.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
